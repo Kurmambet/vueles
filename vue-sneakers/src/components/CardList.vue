829f90cc5e29029b7027cdb5a18e3589cdd77760
Petr <pitr.vishnyakovv.05@gmail.com>
2025-11-16 20:13:38 +0500

    card is done

---
+++ vue-sneakers/src/components/CardList.vue
@@ -0,0 +1,21 @@
+<script setup>
+import Card from './Card.vue'
+
+const onClickAdd = () => {
+    alert(111)
+}
+</script>
+
+<template>
+  <div class="grid grid-cols-4 gap-5">
+    <Card
+      image-url="/sneakers/sneakers-1.jpg"
+      title="Мужские кроссовки Nike Blazer Mid Suede"
+      :price="1205"
+      :is-added="false"
+      :is-favorite="true"
+      :onClickAdd="onClickAdd"
+    />
+
+  </div>
+</template>
